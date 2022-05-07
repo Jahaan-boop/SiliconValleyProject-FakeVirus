b6156b5e265ea1a02ca688116e0d89d90ecd6191
Jahaan-boop <85781919+Jahaan-boop@users.noreply.github.com>
2022-05-07 11:26:35 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function virus(){
+window.location.href="virus_page.html"
+};
+
+console.error("The rating for this site is -2. Proceed with caution and NEVER give out your personal data.");
+
+function reveal(){
+window.location.href="reveal.html"
+}
